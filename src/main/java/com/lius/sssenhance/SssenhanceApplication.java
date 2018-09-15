64a707b0c924f87d39b9fecad89f6e9c7dcbe3b1
liushuai <liushuainudt@foxmail.com>
2018-09-15 11:35:02 +0800

    Add api login check

--- src/main/java/com/lius/sssenhance/SssenhanceApplication.java
+++ src/main/java/com/lius/sssenhance/SssenhanceApplication.java
@@ -1,13 +1,19 @@
 package com.lius.sssenhance;
 
+import com.lius.sssenhance.user.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SssenhanceApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SssenhanceApplication.class);
+
 
     public static void main(String[] args) {
 
         SpringApplication.run(SssenhanceApplication.class, args);
+        logger.info("SssenhanceApplication Started Succesfully!");
     }
 }
