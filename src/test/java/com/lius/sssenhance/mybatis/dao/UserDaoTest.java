21bd0bf492fed27bcc19fe1dacecfe051b6e11ae
liushuai <liushuainudt@foxmail.com>
2018-09-15 11:35:34 +0800

    Add api login check

--- src/test/java/com/lius/sssenhance/mybatis/dao/UserDaoTest.java
+++ src/test/java/com/lius/sssenhance/mybatis/dao/UserDaoTest.java
@@ -9,7 +9,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 
 import com.lius.sssenhance.mybatis.domain.User;
 
@@ -28,6 +32,40 @@ public class UserDaoTest {
         assertThat(user.getUsername()).isEqualTo("刘帅");
         assertThat(user.getPasswd()).isEqualTo("passwd");
         assertThat(user.getLoginIp()==null);
+    }
+
+
+    @Test
+    public void updateUser() {
+        User user = userDao.getUserById(1);
+
+        //get ip
+        String ip1 =null;
+        String ip2 =null;
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ip1 = socket.getLocalAddress().getHostAddress();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        try {
+             ip2 =InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(ip1);
+        System.out.println(ip2);
+
+        user.setLoginIp(ip2);
+
+        int i =userDao.updateUser(user);
+        assertThat(i==1);
+
+
     }
 
 
